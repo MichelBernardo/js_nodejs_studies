e450983df7321b85f416e88f7a1b1a9d5baf733f
Michel Bernardo <mbpaiva@cpqd.com.br>
2025-11-12 22:39:34 -0300

    feat:  adding studies on data types.

---
+++ src/list_variables_functions_types/functions.js
@@ -0,0 +1,32 @@
+/*
+1. Create a function that receives a person's name as an argument and returns a personalized greeting.
+Then, call the function and display the greeting in the console.*/
+
+
+
+/*
+2. Create a function that receives a person's age and returns whether they are an adult (age >= 18).
+Print the result in the console.*/
+
+
+
+/*
+3. Create a function that receives a string and checks whether it is a palindrome (a word that reads the same backward and forward)
+using the string reverse() method. Return true if it is a palindrome and false otherwise.
+Print the result in the console.*/
+
+
+
+/*
+4. Create a function that receives three numbers as parameters and determines which one is the largest.
+Use conditional structures (if, else) to compare the values and determine the largest.
+Print the largest value in the console.*/
+
+
+
+/*
+5. Create an arrow function called calculaPotencia that receives two parameters: the base and the exponent.
+The function should calculate the base raised to the exponent and return the result.
+*/
+
+
---
+++ src/list_variables_functions_types/operators.js
@@ -0,0 +1,34 @@
+/*
+1. Create variables that store the value of the balance, deposit, and withdrawal of a bank account,
+then perform addition and subtraction operations between them using a variable called operation,
+considering the function of each variable created. Display the final results in the console.*/
+
+
+
+/*
+2. Create a numeric variable with a value. Use a ternary operator to check whether this value is even or odd.
+Display the result in the console.*/
+
+
+
+/*
+3. Consider a situation where you are checking if a user is logged in and has admin permission to access a specific feature.
+Use boolean variables to simulate these conditions and use the AND operator to verify if both are true.
+*/
+
+
+
+/*
+4. Declare two boolean variables and use the OR operator to check if at least one of them is true.
+Display the result in the console.
+*/
+
+
+
+/*
+5. Imagine a system that determines whether a user is old enough to buy a ticket for a show.
+Declare two variables that determine the minimum age required and the user’s age,
+and use conditional structures (if, else) to determine if the user can make the purchase.
+*/
+
+
